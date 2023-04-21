3d7293690981b0bf619b2b6717fd489cd13b9238
Gage Poling <gagepoling01@gmail.com>
2023-04-21 11:19:58 -0400

    Create APIService.java

---
+++ src/main/java/Senior/Project/APIService.java
@@ -0,0 +1,29 @@
+package Senior.Project;
+
+import com.merakianalytics.orianna.types.common.Region;
+import com.merakianalytics.orianna.types.core.match.Timeline;
+import com.merakianalytics.orianna.types.core.spectator.CurrentMatch;
+import com.merakianalytics.orianna.types.core.summoner.Summoner;
+
+
+public class APIService {
+	
+	public long getCurrentMatchID(String summonerName) {
+		Summoner summoner = Summoner.named(summonerName).withRegion(Region.NORTH_AMERICA).get();
+
+		CurrentMatch match = CurrentMatch.forSummoner(summoner).get();
+		
+		System.out.println("Live Match ID: " + match.getId());
+		return match.getId();
+	}
+	
+	
+	public Timeline getMatchTimeline(long matchID) {
+		Timeline timeline = Timeline.withId(matchID).withRegion(Region.NORTH_AMERICA).get();
+		
+		System.out.println(timeline);
+		return timeline;
+	}
+}
+
+
